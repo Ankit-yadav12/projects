dfb01c7a4c6b33426c7091b0c3f44cdca70f82d8
Ankit Yadav <py90398@gmail.com>
2025-03-26 12:53:58 +0530

    completed the project on random color generator

--- background Changer/script.js
+++ background Changer/script.js
@@ -1,13 +1,16 @@
 let box = document.querySelector('#box');
 let btn = document.querySelector('.btn');
 let colorCode = document.querySelector('#colorCode');
+let rotationAngle = 0;
 
-btn.addEventListener('click',  function(){
+btn.addEventListener('click', function () {
+    let a = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+    let c = Math.floor(Math.random() * 256);
 
-    let a = Math.floor(Math.random() *256);
-    let b = Math.floor(Math.random() *256);
-    let c = Math.floor(Math.random() *256);
+    rotationAngle += 1800;
 
+    box.style.transform = `rotate(${rotationAngle}deg)`;
     colorCode.textContent = `RGB(${a},${b},${c})`;
     box.style.backgroundColor = `rgb(${a},${b},${c})`;
 });
