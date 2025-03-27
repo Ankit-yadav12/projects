da579fd0d4ed90f343445dbe08c3fee3e4545687
Ankit Yadav <py90398@gmail.com>
2025-03-27 19:50:44 +0530

    completed task 3

---
+++ practice like hell/3 form validation/script.js
@@ -0,0 +1,16 @@
+let list = document.querySelectorAll('form input[type="text"]');
+let btn = document.querySelector('#btn');
+
+btn.addEventListener('click', function(event) {
+     event.preventDefault();
+
+     for (let i = 0; i < list.length; i++) {
+         if (list[i].value === '') {
+             list[i].style.backgroundColor = "red";
+             list[i].style.color = "white";
+         } else {
+             list[i].style.backgroundColor = ""; // Reset background
+             list[i].style.color = ""; // Reset text color
+         }
+     }
+});
