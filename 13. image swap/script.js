aade8e1efce8566db720256835c54d51e376e5ac
Ankit Yadav <py90398@gmail.com>
2025-04-05 13:02:57 +0530

    made changes in image swapper

--- 13. image swap/script.js
+++ 13. image swap/script.js
@@ -4,9 +4,15 @@ let img2 = document.querySelector('#img2');
 let btn = document.querySelector('#btn');
 
 btn.addEventListener('click',function(){
-    let temp =  img1.getAttribute('src');
-    let temp2 = img2.getAttribute('src');
+    //button animation
+    btn.style.transition='transform 0.1s ease-in-out';
+    btn.style.scale='0.9';
+    setTimeout(function(){
+        btn.style.scale='1';
+    },100)
 
-    img1.setAttribute('src',temp2);
+    //image swap
+    let temp =  img1.getAttribute('src');
+    img1.setAttribute('src',img2.getAttribute('src'));
     img2.setAttribute('src',temp);
 })
